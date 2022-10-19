c60cfc8e4dff3cf1c4a098537ed3a32fa8f39229
dr_blobfish <jules.herrmann@gmail.com>
2022-10-19 10:32:03 +0200

    first attempt at exo 1

---
+++ TP6/tp6-1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define STR_MAX 30
+
+
+void invert_maj_min(char *input_str, char *output_str){
+    int str_not_finished = 1;
+    for (int i=0;str_not_finished && i<STR_MAX;i++){
+        char letter = *(input_str+i);
+        if (letter == '\0'){
+            str_not_finished = 0;
+        }
+        if (letter >= 65 && letter <=90){
+            letter = letter + 32;
+        }
+        else if (letter >= 97 && letter <= 122){
+            letter = letter - 32;
+        }
+        *(output_str+i) = letter;
+    }
+}
+
+
+int main(){
+
+    char ch_input[STR_MAX],ch_output[STR_MAX];
+    printf("entrez une chaine : ");
+    fgets(ch_input,STR_MAX,stdin);
+
+    invert_maj_min(ch_input,ch_output);
+
+    printf("%s",ch_output);
+
+    return EXIT_SUCCESS;
+}
