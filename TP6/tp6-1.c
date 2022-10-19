087878fa7559ecfaf3a8b817594b424d7c08a07f
dr_blobfish <jules.herrmann@gmail.com>
2022-10-19 10:36:45 +0200

    improved from correction

--- TP6/tp6-1.c
+++ TP6/tp6-1.c
@@ -8,17 +8,17 @@
 void invert_maj_min(char *input_str, char *output_str){
     int str_not_finished = 1;
     for (int i=0;str_not_finished && i<STR_MAX;i++){
-        char letter = *(input_str+i);
+        char letter = input_str[i];
         if (letter == '\0'){
             str_not_finished = 0;
         }
-        if (letter >= 65 && letter <=90){
-            letter = letter + 32;
+        if (letter >= 'A' && letter <='Z'){
+            letter = letter + 'a'-'A';
         }
-        else if (letter >= 97 && letter <= 122){
-            letter = letter - 32;
+        else if (letter >= 'a' && letter <= 'z'){
+            letter = letter - 'a'+'A';
         }
-        *(output_str+i) = letter;
+        output_str[i] = letter;
     }
 }
 
